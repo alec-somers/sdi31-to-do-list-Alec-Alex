752f402ed4a50ed3791926cb0956fb1e7606a325
alec-somers <alecv1528@gmail.com>
2025-07-09 13:19:34 -0700

    initial js and css made

---
+++ index.js
@@ -0,0 +1,23 @@
+let btn = document.querySelector(".add-button");
+let input = document.querySelector(".to-do-input");
+let unorderedList = document.querySelector("ul");
+let removeButtons = document.getElementsByClassName("remove-button");
+
+btn.addEventListener("click", () => {
+  let newListItem = document.createElement("li");
+  newListItem.innerHTML =
+    '<input class="checkbox" type="checkbox"/> <label>' +
+    input.value +
+    '</label> <button type="button" class="remove-button">Remove</button>';
+
+  unorderedList.appendChild(newListItem);
+
+  for (let i = 0; i < removeButtons.length; i++) {
+    let currentRemoveButton = removeButtons[i];
+    let currentListItem = unorderedList.children[i];
+
+    currentRemoveButton.addEventListener("click", (event) => {
+      event.target.parentElement.remove();
+    });
+  }
+});
